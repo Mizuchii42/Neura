8dfa1faff4a0f2667c3540391301338c48326394
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-05 21:56:55 +0700

    cmd buff

--- src/modul/cmdControls.js
+++ src/modul/cmdControls.js
@@ -1,9 +1,14 @@
-import { menuMessage } from "../config/variabel.js";
+import { buffMessage, menuMessage } from "../config/variabel.js";
 import { isBan } from "../plugins/fitur/ban.js"
 export const cmdMenucontrol = (sock, chatId, msg, text) => {
   if (text.startsWith("!menu")) {
     if (isBan(sock, chatId, msg)) return;
     sock.sendMessage(chatId, { text: menuMessage }, { quoted: msg });
   }
+  if (text.startsWith("!buff")) {
+    if (isBan(sock, chatId, msg)) return;
+    sock.sendMessage(chatId, { text: buffMessage }, { quoted: msg });
+  }
+
 
 }
