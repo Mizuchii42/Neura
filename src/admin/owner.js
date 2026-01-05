b1c23d4b5f3b62ca64b92eff72c1e22ac24b5303
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-05 18:34:49 +0700

    first commit

--- src/admin/owner.js
+++ src/admin/owner.js
@@ -2,7 +2,7 @@ import { ban, isOwner } from "../plugins/fitur/ban.js"
 
 export const ownerControls = async (sock, chatId, msg, text) => {
   if (text.startsWith("!ban")) {
-    if (!isOwner(sock, chatId, msg)) return;
+    if (!isOwner(sock, msg, chatId)) return;
     ban(sock, chatId, msg);
 
   }
