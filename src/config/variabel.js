7b019aceb1a73761e8e40ff22fc228f0dcd9409a
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-09 11:01:57 +0700

    cmd any

--- src/config/variabel.js
+++ src/config/variabel.js
@@ -2,7 +2,7 @@ export const menuMessage = `
 *Neura Bot*\n> gunakan bot dengan bijak, dukung bot agar bisa hidup lebih lama dengan donet seiklasnya
 vps exp: 21 januari 2026
 donate: https://sociabuzz.com/neurabot/tribe
-grub: https://chat.whatsapp.com/IeRyRhLn7OA5lKSrJnBlVv
+komunitas: https://whatsapp.com/channel/0029VbAmhBF7z4kjOQcoVV0A
 
 *Cmd Admin*
 - !kick
