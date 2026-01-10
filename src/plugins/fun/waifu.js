2af79e30165ffac3c26a3e61cc0f30b198728d30
dimasyoga42 <dimasyoga9090@gmail.com>
2026-01-10 07:00:32 +0700

    cmd any

--- src/plugins/fun/waifu.js
+++ src/plugins/fun/waifu.js
@@ -1,12 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
-export const waifu = async (sock, chtId, msg) => {
+export const waifu = async (sock, chatId, msg) => {
   try {
-    const data = await axios.get('https://api.waifu.pics/sfw/waifu')
-    const imgdata = data.data.url
-    if (!imgdata) return sock.sendMessage(chtId, { text: "gagal mengambil foto" }, { quoted: msg })
-    sock.sendMessage(chtId, { image: url(`${imgdata}`) }, { quoted: msg });
+    const { data } = await axios.get("https://api.waifu.pics/sfw/waifu");
+
+    if (!data?.url) {
+      return sock.sendMessage(
+        chatId,
+        { text: "gagal mengambil foto" },
+        { quoted: msg }
+      );
+    }
+
+    await sock.sendMessage(
+      chatId,
+      {
+        image: { url: data.url },
+        caption: "waifu"
+      },
+      { quoted: msg }
+    );
+
   } catch (err) {
-    sock.sendMessage(`error internal server\nlog message:${err}`)
+    await sock.sendMessage(
+      chatId,
+      { text: `error internal server\n${err.message}` },
+      { quoted: msg }
+    );
   }
-}
+};
